040b69a560d61434f5fabaaafbf92d34479a3cbd
Bedriye Şahin <hbedriyesahin@gmail.com>
2022-12-15 22:39:37 +0300

    created task1

---
+++ src/Task1.java
@@ -0,0 +1,25 @@
+public class Task1 {
+    String str = "amazon";
+
+        System.out.println(reverseString(str));
+
+}
+
+    public static String reverseString(String str) {
+
+        String newString="";
+        char c;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            c = str.charAt(i);
+
+            if (i!=str.length()-1)
+                newString = "-" + c + newString;
+            else
+                newString= c + newString;
+
+        }return newString;
+
+    }
+}
